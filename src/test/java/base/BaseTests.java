3d84156de3b331c6c15325416d637f591145b250
Ahmed Ali <Ahmed98148@gmail.com>
2021-01-31 19:42:47 -0800

    Login test using TestNG

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,29 +4,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
-    private WebDriver driver;
 
+    private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
+        homePage = new HomePage(driver);
 
-        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        inputsLink.click();
+    }
 
-        System.out.println(driver.getTitle());
+    @AfterClass
+    public void tearDown(){
         driver.quit();
     }
 
-    public static void main(String arg[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
 }
